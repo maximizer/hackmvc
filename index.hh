03299c0ca80d41685bedcad8a48031a2f02e2e08
root <patrick@sparefoot.com>
2014-03-31 17:58:53 -0500

    Removed controller

--- index.hh
+++ index.hh
@@ -3,8 +3,6 @@ require __DIR__.'/src/Controller.hh';
 require __DIR__.'/src/Route.hh';
 require __DIR__.'/src/Router.hh';
 
-$c = new Controller();
-$get = $c->GETParams();
 
 $router = new Router();
 $router->registerRoute("/foo", new Route("Foo.php", "index"));
